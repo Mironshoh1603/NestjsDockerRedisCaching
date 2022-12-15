5c112b5bbb1942e11a96a878d6b84e080146a961
mironshoh1603 <mironshohasadov1603@gmail.com>
2022-12-15 12:29:16 +0500

    ready redis

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller,CACHE_MANAGER, Get,UseInterceptors,CacheInterceptor,CacheTTL,CacheKey } from '@nestjs/common';
+import { Inject } from '@nestjs/common/decorators';
 import { AppService } from './app.service';
+import { getCats, getDogs } from './utills';
+import {Cache} from "cache-manager"
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,
+    @Inject(CACHE_MANAGER) private cacheManager:Cache) {
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  }
+
+  @UseInterceptors(CacheInterceptor)
+  @CacheTTL(10)
+  @CacheKey("all-dogs")
+  @Get("dogs")
+  getDogs() {
+    return getDogs()
+  }
+
+
+  @Get("cats")
+  async getCats() {
+    const cachedCats = await this.cacheManager.get(
+      "all-cats"
+    );
+    if (cachedCats) return cachedCats;
+const cats = await getCats();
+    this.cacheManager.set("all-cats", cats,{ttl:10} as any);
+return cats;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
+import { redisStore } from 'cache-manager-redis-store';
+import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register({
+      // @ts-ignore
+      store: async () => await redisStore({
+        // Store-specific configuration:
+        socket: {
+          host: 'localhost',
+          port: 6379,
+        }
+      })
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
